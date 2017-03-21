dd941c4fda331672eb19940289b8420ca1b69217
mapengzhen <mapengzhen@rd.tuan800.com>
2017-03-21 16:09:56 +0800

    粒子效果

--- js/main.js
+++ js/main.js
@@ -14,9 +14,6 @@
    	// will first fade out the loading animation
     	$("#status").fadeOut("slow");
 
-    	// will fade out the whole DIV that covers the website.
-    	$("#preloader").delay(5).fadeOut("slow").remove();     
-
     	$('.js #hero .hero-image img').addClass("animated fadeInUpBig");
       $('.js #hero .buttons a.trial').addClass("animated shake");
 
